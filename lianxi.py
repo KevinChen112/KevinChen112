bda077199b6d5fce8158a03960eb4d890d34accc
KevinChen112 <1127027469@qq.com>
2025-04-17 04:20:26 +0800

    Update lianxi.py

--- lianxi.py
+++ lianxi.py
@@ -1,21 +1,21 @@
-import requests
+进口要求
 
-url1 = 'https://helmet.real2tech.net/api/label/PC/labelInfo/page?pageSize=20&pageNo=1'
-payload = {"labelType":"","mac":"806EC9","projectId":"1867092082510057474"}
-headers = {'Authorization': 'Bearer eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsInVzZXJJZCI6IjEiLCJuYW1lIjoiYWRtaW4iLCJpZCI6Inc2bndQOXVuIiwiZGVwYXJ0SWQiOiIxMDAiLCJleHAiOjE3NDQ5MDE3MDN9.YwoETE7D8hOAbazBwEx5Qi2LDJMydl4jqkm0J1ODexX82PnuPCeAvAEIoybKWo6DpkmH7dsE1eYHmbGeioRJeCilBEaFiieY--3demjWXK9Tf7MmKLPVS2ignJs4nDU8cN9a2kWUbG7I6MAB7axVrNQhfXktJAwBkGe5gmJAIAM','Content-Type':'application/json;charset=UTF-8','Cookie':'Hm_lvt_1dfa88d0c8571eef2d383070cb3e5ca1=1744815323; HMACCOUNT=8ED526F9A34A420B; d2admin-1.5.9-isRefresh=false; d2admin-1.5.9-uuid=1; d2admin-1.5.9-token=eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsInVzZXJJZCI6IjEiLCJuYW1lIjoiYWRtaW4iLCJpZCI6Inc2bndQOXVuIiwiZGVwYXJ0SWQiOiIxMDAiLCJleHAiOjE3NDQ5MDE3MDN9.YwoETE7D8hOAbazBwEx5Qi2LDJMydl4jqkm0J1ODexX82PnuPCeAvAEIoybKWo6DpkmH7dsE1eYHmbGeioRJeCilBEaFiieY--3demjWXK9Tf7MmKLPVS2ignJs4nDU8cN9a2kWUbG7I6MAB7axVrNQhfXktJAwBkGe5gmJAIAM; d2admin-1.5.9-expireDate=2025-04-17%2022:55:03; d2admin-1.5.9-refreshToken=w6nwP9un; d2admin-1.5.9-tenantId=null; d2admin-1.5.9-projectId=1867092082510057474; d2admin-1.5.9-isChange=false; d2admin-1.5.9-canReturn=true; Hm_lpvt_1dfa88d0c8571eef2d383070cb3e5ca1=1744815345'}
-def post_request(url,payload,headers):
-    response = requests.post(url, json=payload, headers=headers)
-    if response.status_code == 200:
-        data = response.json()  # 如果响应是 JSON 格式
-        print(data)
-    else:
-        print(f"请求失败，状态码: {response.status_code}")
+url1 ='https://helmet.real2tech.net/api/label/PC/labelInfo/page？' pageSize=20&pageNo=1 '
+有效负载={"标签类型":"","苹果":" 806EC9 ","项目Id ":"1867092082510057474"}
+标题={'授权': 无记名eyjhbgcioijsuzi 1 nij 9 . eyjzdwiioijhzg 1 pbiisinvzzxjzci 6 ijeeilcjuyw 1 lijoiywrtaw 4 ilcjpzci 6 Inc 2 bndquoxvuiiwizgvwyxj 0 swqioiixmdailcjehaioje 3 nd qd 5 MDN 9 . yweet 7d 8 hoabazbwex 5 qi2 ldjmydl 4 jqkm 0j odex 82 pnupceavaeibkwo 6 dpk MH 7 ds,'内容类型':应用/JSON；charset=UTF-8 ',饼干:hm _ lvt _ 1 DFA 88d 0 c 8571 eef 2d 383070 CB 3 e 5 ca 1 = 1744815323；hm account = 8ed 526 F9 a 34 a 420 b；D2 admin-1 . 5 . 9-is refresh = false；D2 admin-1 . 5 . 9-uuid = 1；D2 admin-1 . 5 . 9-token = eyjhbgcioijsuzi 1 nij 9 . eyjzdwioijzg 1 pbiisisinvzzxjzci 6 ije ilcjuyw 1 lijoiywrtaw 4 ilcjpzci 6 Inc 2 bndquoxvuiiwizzg vwyxj 0 swqioiiximdailcjlehaioje 3 ndq 5 MDN 9 . yweet 7d 8 hoabazbwex 5 qi 2 ldjmydl 4 jqkm0 j 1 odex 82 pnupceavaeid2admin-1 . 5 . 9-expire date = 2025-04-17% 2022:55:03;D2 admin-1 . 5 . 9-refresh token = w 6 NWP 9 un；D2 admin-1 . 5 . 9-tenantId = null；D2 admin-1 . 5 . 9-projectId = 1867092082510057474；D2 admin-1 . 5 . 9-is change = false；D2 admin-1 . 5 . 9-can return = true；hm _ lpvt _ 1 DFA 88 d0c 8571 eef 2d 383070 CB 3 e5ca 1 = 1744815345 '}
+极好的 post _请求(url、有效负载、标题):
+响应=请求。邮政(url，json =有效负载，headers =标题)
+    如果回应。状态代码 == 200:
+数据=响应。json()  # 如果响应是JSON格式
+        打印(数据)
+    其他:
+        打印(f "请求失败,状态码：{回应。状态代码}")
 
-url2 = 'https://helmet.real2tech.net/api/passage/projectOrgan/getOrganTree?projectId=1867092082510057474'
-response = requests.get(url2, headers=headers)
-if response.status_code == 200:
-    data = response.json()  # 如果响应是 JSON 格式
-    print(data)
-else:
-    print(f"请求失败，状态码: {response.status_code}")
-    print('123454353464564562153153123153214')
+url2 =https://helmet . real 2 tech . net/API/passage/project organ/getOrganTree？projectId=1867092082510057474 '
+响应=请求。得到(url2，标题=标题)
+如果回应。状态代码 == 200:
+数据=响应。json()  # 如果响应是JSON格式
+    打印(数据)
+其他:
+    打印(f "请求失败,状态码：{回应。状态代码}")
+    打印('123454353464564562153153123153214')
